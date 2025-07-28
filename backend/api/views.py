6dcbd7d8dad0353f20c187c1ace44509ef0fd460
Sasank5716 <sasanklama5716@gmail.com>
2025-07-28 14:38:42 +0545

    change class view name NoteListView -> NoteListCreate

--- backend/api/views.py
+++ backend/api/views.py
@@ -5,7 +5,7 @@ from .serializers import UserSerializer, NoteSerializer
 from rest_framework.permissions import IsAuthenticated, AllowAny
 from .models import Note
 
-class NoteListView(generics.ListCreateAPIView):
+class NoteListCreate(generics.ListCreateAPIView):
     serializer_class = NoteSerializer
     permission_classes = [IsAuthenticated]
 
