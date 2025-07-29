c0168f0f392394d6a4e6e90cd16e3b94a6225c37
Sasank5716 <sasanklama5716@gmail.com>
2025-07-29 17:31:56 +0545

    update layout of NotFound page

--- frontend/src/pages/NotFound.jsx
+++ frontend/src/pages/NotFound.jsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 function NotFound() {
   return (
-    <div>NotFound</div>
+    <div>
+      <h1>404 - Page Not Found</h1>
+      <p>The page you are looking for does not exist.</p>
+    </div>
+    
   )
 }
 
