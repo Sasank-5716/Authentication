6c5659bdb5c18c48e6adac3b99e1919db7525530
Sasank5716 <sasanklama5716@gmail.com>
2025-07-29 17:30:30 +0545

    create pages and setup routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,31 @@
 import react from 'react'
+import {BrowserRouter, Router, Route, Navigate, Routes} from 'react-router-dom'
+import Home from './pages/home'
+import Login from './pages/login'
+import NotFound from './pages/notfound'
+import Register from './pages/register'
+import ProtectedRoute from './components/ProtectedRoute'
+
+function Logout(){
+  localStorage.clear()
+  return <Navigate to="/login" replace />
+}
+
+function RegisterAndLogout() {
+  localStorage.clear()
+  return <Register/>
+}
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      
-    </>
+return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<ProtectedRoute><Home/></ProtectedRoute>}/>
+        <Route path='/login' element={<Login/>}/>
+        <Route path='*' element={<NotFound/>}/>
+        <Route path='/register' element={<Register/>}/>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
